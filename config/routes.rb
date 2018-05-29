7c23e920765c56b22684090c6121c5a0d07cfa9d
Alfonso <ajzuniga1@miuandes.cl>
2018-05-29 15:06:12 +0000

    All done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :comments
   devise_for :users, controllers: { :omniauth_callbacks => 'callbacks' }
   resources :movies
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
